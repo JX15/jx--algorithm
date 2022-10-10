3b0fe4a247a8a8dd3406b4f8db92ab349cdbcb02
jiangxin <emailjx@163.com>
2022-10-10 17:09:05 +0800

    feat：体系班-选择排序2

--- src/main/java/com/jiangxin/demo/algorithm/basic/class01/SelectionSort.java
+++ src/main/java/com/jiangxin/demo/algorithm/basic/class01/SelectionSort.java
@@ -7,12 +7,14 @@ import com.jiangxin.demo.algorithm.utils.MyArrayUtil;
  * @DATE: 2022年10月10日 12:51:04
  * @Description: 选择排序，目标：5分钟之内写出正确且最优解
  * 20221010：25分钟，第一次没写对，第二次写对了但不是最优解，第三次看了标准答案才磕磕绊绊改完
+ * 20221010: 2分13秒，没有做基础判断
  */
 public class SelectionSort {
 
 
     public static int[] doSort(int[] arr) {
-        //todo-jx 继续练
+        //todo-jx 第二天继续练一把，达标就下一题
+
         return arr;
     }
 
