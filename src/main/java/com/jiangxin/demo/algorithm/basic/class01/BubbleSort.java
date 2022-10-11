46aac8d17f5fe837638ab83ef6c97056d3f2e022
jiangxin <emailjx@163.com>
2022-10-11 09:56:46 +0800

    feat：体系班-冒泡排序

---
+++ src/main/java/com/jiangxin/demo/algorithm/basic/class01/BubbleSort.java
@@ -0,0 +1,33 @@
+package com.jiangxin.demo.algorithm.basic.class01;
+
+import com.jiangxin.demo.algorithm.utils.MyArrayUtil;
+
+/**
+ * @Author: xin.jiang.cn
+ * @DATE: 2022年10月11日 09:45:15
+ * @Description: 冒泡排序，目标：5分钟之内写出最优解
+ * 20221011: 10分钟左右，看了最优解
+ */
+public class BubbleSort {
+
+    public static int[] doSort(int[] arr){
+        if (arr == null || arr.length < 2){
+            return arr;
+        }
+        for (int i = arr.length - 1; i > 0 ; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]){
+                    MyArrayUtil.swap(arr, j, j+1);
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] randomArray = MyArrayUtil.getRandomArray(10, 100);
+        MyArrayUtil.printArray(randomArray);
+        int[] sortedArr = doSort(randomArray);
+        MyArrayUtil.printArray(sortedArr);
+    }
+}
